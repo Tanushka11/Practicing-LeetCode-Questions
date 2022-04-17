d0189a809657f2741f96852478409b37cb8bb58c
Tanushka Lalwani <90191834+Tanushka11@users.noreply.github.com>
2022-04-17 17:30:30 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (93.31%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.java
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -1,12 +1,12 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
-        int[] ans=new int[2*n];
-        int j=0;
-        for(int i=0;i<ans.length;i+=2){
-        ans[i]=nums[j];
-            ans[i+1]=nums[j+n];
-            j++;
+        for(int i=0;i<n;i++){
+          nums[i+n]+=nums[i]*10000;
         }
-        return ans;
+        for(int i=0;i<n;i++){
+              nums[2*i]=nums[i+n]/10000;
+            nums[2*i+1]=nums[i+n]%10000;
+        }
+        return nums;
     }
 }
