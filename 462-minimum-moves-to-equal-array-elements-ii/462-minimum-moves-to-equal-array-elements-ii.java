02d1994a62fe5983ce09f05594d658d915190c62
Tanushka Lalwani <90191834+Tanushka11@users.noreply.github.com>
2022-06-30 20:47:13 +0530

    Time: 7 ms (30.83%), Space: 45.9 MB (42.98%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
@@ -4,11 +4,27 @@ class Solution {
         int count=0;
         int start=0;
         int end=nums.length-1;
-        while(start<end){
-         count +=nums[end] - nums[start];
-            start++;
-            end--;
+        // while(start<end){
+        //  count +=nums[end] - nums[start];
+        //     start++;
+        //     end--;
+        // }
+        // return count;
+       
+        
+        int median;
+        if(nums.length % 2 !=0){
+            median=nums[nums.length/2];
         }
-        return count;
+        else{
+            median= Math.round((nums[nums.length/2] + nums[nums.length/2 -1])/2);
+        }
+        while(start<=end){
+         count +=Math.abs(median-nums[start]);
+            start++;
+            
     }
+       return count;   
+}
+  
 }
