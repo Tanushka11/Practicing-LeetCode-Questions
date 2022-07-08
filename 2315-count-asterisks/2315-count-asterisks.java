f7b008fc923b84cdd0e3d5e0e9808a75f5aec1a2
Tanushka Lalwani <90191834+Tanushka11@users.noreply.github.com>
2022-07-08 16:10:34 +0530

    Time: 3 ms (50.64%), Space: 41.9 MB (85.23%) - LeetHub

--- 2315-count-asterisks/2315-count-asterisks.java
+++ 2315-count-asterisks/2315-count-asterisks.java
@@ -1,16 +1,16 @@
 class Solution {
     public int countAsterisks(String s) {
-        boolean bars=false;
+        int bars=0;
         int count =0;
         for(int i=0;i<s.length();i++){
             if(s.charAt(i)=='|'){
-               bars=!bars;
+             bars++;
             }
-            if(!bars && s.charAt(i)=='*'){
+            if(bars%2==0 && s.charAt(i)=='*'){
                 count ++;
             }
             
         }
         return count;
     }
-}
+} 
