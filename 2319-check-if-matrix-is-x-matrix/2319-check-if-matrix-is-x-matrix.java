3c36b4bd0cf90c3d7049fe0c7990ba8416592327
Tanushka Lalwani <90191834+Tanushka11@users.noreply.github.com>
2022-07-08 17:19:37 +0530

    Time: 4 ms (19.21%), Space: 54.8 MB (24.24%) - LeetHub

---
+++ 2319-check-if-matrix-is-x-matrix/2319-check-if-matrix-is-x-matrix.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean checkXMatrix(int[][] grid) {
+     for(int i=0;i<grid.length;i++){
+         for(int j=0;j<grid[i].length;j++){
+             
+             if( i==j ||( i+j)==grid.length-1 ){
+                 if(grid[i][j]==0){
+                     return false;
+                 }
+                 
+             }
+             else{
+                 if(grid[i][j]!=0){
+                     return false;
+                 }
+             }
+         }
+     }
+        return true;
+    }
+}
