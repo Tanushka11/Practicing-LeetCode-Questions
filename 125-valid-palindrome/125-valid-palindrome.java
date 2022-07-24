5c9304430db7a85ece57a49b6fa4b61be326273f
Tanushka Lalwani <90191834+Tanushka11@users.noreply.github.com>
2022-07-24 19:50:54 +0530

    Time: 4 ms (89.31%), Space: 43 MB (85.72%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -3,32 +3,28 @@ class Solution {
         if(s.isEmpty()){
             return true;
         }
-        int start = 0;
-        int end = s.length() - 1 ; 
-        
-        
-        while(start < end){
+        int start=0;
+        int end=s.length() - 1;
+        while(start<end){
             char c1 = s.charAt(start);
-        char c2 = s.charAt(end);
-        
-          if(!Character.isLetterOrDigit(c1)){
-              start++;
-          } 
-          else if (!Character.isLetterOrDigit(c2)){
-              end--;
-          }  
-          else{
-          
-         if(Character.toLowerCase(c1) != Character.toLowerCase(c2)){
-            return false;
-        }
-            else{
+            char c2 = s.charAt(end);
+            
+            if(!Character.isLetterOrDigit(c1)){
                 start++;
-                end--;
+            }
+            else if(!Character.isLetterOrDigit(c2)){
+                end--; 
+            }
+            else{
+                if(Character.toLowerCase(c1)!= Character.toLowerCase(c2)){
+                    return false;
+                }
+                else{
+                    start++;
+                    end--;
+                }
             }
         }
-        }
-            
         return true;
     }
 }
