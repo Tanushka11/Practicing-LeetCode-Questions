316b1a60b834e9744795cd2fc99faa435ae02810
Tanushka Lalwani <90191834+Tanushka11@users.noreply.github.com>
2022-09-03 20:50:21 +0530

    Time: 15 ms (49.99%), Space: 44.3 MB (69.16%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.java
+++ 9-palindrome-number/9-palindrome-number.java
@@ -1,17 +1,12 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if(x<0 || (x!=0 && x%10==0)){
-           return false;
+        int rev = 0;
+        int og = x;
+        while(x > 0){
+            int rem = x % 10;
+            rev = rev *10 + rem;
+            x = x/10;
         }
-        else if(x<=9){
-            return true;
-        }
-       int ans=0;
-        while(x>ans){
-            ans= ans *10 + (x%10);
-            x=x/10;
-        }
-        
-        return ((x==ans)|| x==ans/10);
+        return (og==rev);
     }
 }
