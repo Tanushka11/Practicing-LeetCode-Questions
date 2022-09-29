eba2e48e0e5cd7770f30376fad0039ffadf47ce7
Tanushka Lalwani <90191834+Tanushka11@users.noreply.github.com>
2022-09-29 21:17:52 +0530

    Time: 28 ms (23.97%), Space: 69 MB (58.28%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.java
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -1,12 +1,11 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         Arrays.sort(nums);
-        for(int i=0;i<nums.length-1;i++){
-            if(nums[i]==nums[i+1]){
-                return true;
-            }
-            
-        }
+       for(int i=0;i<nums.length-1;i++){
+           if(nums[i] == nums[i+1]){
+               return true;
+           }
+       }
         return false;
     }
 }
