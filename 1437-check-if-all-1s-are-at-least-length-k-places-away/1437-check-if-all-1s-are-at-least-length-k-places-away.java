6a29baab1c74b476332d3f7e25ebcda25ca1db48
Tanushka Lalwani <90191834+Tanushka11@users.noreply.github.com>
2022-10-15 18:27:08 +0530

    Time: 10 ms (11.82%), Space: 75.5 MB (5.20%) - LeetHub

--- 1437-check-if-all-1s-are-at-least-length-k-places-away/1437-check-if-all-1s-are-at-least-length-k-places-away.java
+++ 1437-check-if-all-1s-are-at-least-length-k-places-away/1437-check-if-all-1s-are-at-least-length-k-places-away.java
@@ -1,12 +1,19 @@
 class Solution {
     public boolean kLengthApart(int[] nums, int k) {
       
-    for(Integer previousOneIndex = null,i=0; i<nums.length; i++)
-		if(nums[i]==1) {
-			if(previousOneIndex != null && i-previousOneIndex <= k) return false;
-			previousOneIndex = i;
-		}
-	return true;
-}
-
+     
+        for(Integer previndex = null,i=0;i<nums.length;i++)
+        {
+            if(nums[i] == 1)
+            {
+                if(previndex != null && i-previndex <= k)
+                {
+                    return false;
+                }
+                previndex = i;
+            }
+            
+        }
+        return true;
+    }
 }
