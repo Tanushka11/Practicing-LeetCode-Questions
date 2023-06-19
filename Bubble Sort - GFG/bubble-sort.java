d6440b1bde626ebb6cf9ee655ef5b31b573c0247
Tanushka Lalwani <90191834+Tanushka11@users.noreply.github.com>
2023-06-19 17:30:54 +0530

    Added solution - LeetHub

--- Bubble Sort - GFG/bubble-sort.java
+++ Bubble Sort - GFG/bubble-sort.java
@@ -15,13 +15,19 @@ class Solution
     {
         //code here
         for(int i =n-1; i>=0; i--){
+            int didSwap = 0;
             for(int j = 0; j<i; j++){
                 if(arr[j] > arr[j+1] ){
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
+                    didSwap++;
                 }
             }
+            if(didSwap==0){
+                break;
+            }
+
         }
     }
 }
