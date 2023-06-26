b1752c8dd28fa5379e83ffebcde7a77ea974620f
Tanushka Lalwani <90191834+Tanushka11@users.noreply.github.com>
2023-06-26 20:12:48 +0530

    Added solution - LeetHub

--- Searching an element in a sorted array - GFG/searching-an-element-in-a-sorted-array.java
+++ Searching an element in a sorted array - GFG/searching-an-element-in-a-sorted-array.java
@@ -41,11 +41,12 @@ class Solution{
     {
         
         // Your code here
-        for(int i=0;i<N;i++){
+        int ans = -1;
+        for(int i = 0 ;i<N;i++){
             if(arr[i] == K){
-                return 1;
+                ans = 1;
             }
         }
-         return -1;
+        return ans;
     }
 }
