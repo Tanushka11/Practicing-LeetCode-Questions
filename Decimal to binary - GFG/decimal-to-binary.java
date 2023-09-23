785f3ede8256de6749c97d5fe9891582b5ec788e
Tanushka Lalwani <90191834+Tanushka11@users.noreply.github.com>
2023-09-23 20:07:29 +0530

    Added solution - LeetHub

---
+++ Decimal to binary - GFG/decimal-to-binary.java
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+
+class GFG{
+	public static void main(String [] args) throws IOException{
+		Scanner sc = new Scanner(System.in);
+		int test = sc.nextInt();
+		while(test-- > 0) {
+			int N = sc.nextInt();
+			Solution obj = new Solution();
+			obj.toBinary(N);
+			System.out.println();
+		}
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+	void toBinary(int N) {
+		//Write your code here
+		String ans  = "";
+		while(N!=0){
+		    int r  = N % 2;
+		    ans += r;
+		    N /= 2;
+		}
+		StringBuffer s = new StringBuffer(ans);
+		System.out.print(s.reverse());
+		
+	}
+}
