5a9359bf37db039d494eb4e64b334b883d34f8e5
Tanushka Lalwani <90191834+Tanushka11@users.noreply.github.com>
2024-01-06 17:32:39 +0530

    Time: 0 ms (100.00%) | Memory: 89.7 MB (17.79%) - LeetSync

--- 2047-find-a-peak-element-ii/find-a-peak-element-ii.java
+++ 2047-find-a-peak-element-ii/find-a-peak-element-ii.java
@@ -1,16 +1,38 @@
 class Solution {
     public int[] findPeakGrid(int[][] mat) {
-        int max = -1;
-        int[] ans = new int[2];
-        for(int i = 0; i<mat.length; i++){
-            for(int j=0; j<mat[i].length; j++){
-                if(mat[i][j] > max){
-                    max = mat[i][j];
-                    ans[0] = i;
-                    ans[1] = j;
-                }
+        
+        int start = 0;
+        int end = mat[0].length - 1;
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            int row  = findMaxInRow(mat,mid);
+            int leftElem = -1;
+        int rightElem = -1;
+            if(mid > 0){
+                leftElem = mat[row][mid-1];
+            }
+            if(mid < mat[0].length -1){
+                rightElem = mat[row][mid+1];
+            }
+            if(mat[row][mid] > leftElem && mat[row][mid] > rightElem){
+                return new int[] {row,mid};
+            }else if(mat[row][mid] < leftElem){
+                 end = mid-1;
+            }else{
+                start = mid+1;
             }
         }
-        return ans;
+         return new int[] {-1,-1};
+    }
+    int findMaxInRow(int[][] mat, int c){
+        int max = -1;
+        int ind = -1;
+        for(int r = 0; r < mat.length; r++){
+            if(mat[r][c] > max){
+                ind = r;
+                max = mat[r][c];
+            }
+        }
+        return ind;
     }
 }
