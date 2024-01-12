c5a96de30e58b2d30da85d3b5c896e5f00af778a
Tanushka Lalwani <90191834+Tanushka11@users.noreply.github.com>
2024-01-12 13:12:22 +0530

    Time: 217 ms (5.50%) | Memory: 45.8 MB (5.13%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.java
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.java
@@ -4,7 +4,6 @@ class Solution {
         if(Character.getNumericValue(num.charAt(num.length()-1)) % 2 != 0){
             return num;
         }
-        int max = -1;
         for(int i = num.length() - 1; i>0; i--){
             String s = num.substring(0,i);
 
