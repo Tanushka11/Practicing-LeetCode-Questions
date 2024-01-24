cf25e501e6800f4a334a7d13740cb670e88904a4
Tanushka Lalwani <90191834+Tanushka11@users.noreply.github.com>
2024-01-24 19:00:11 +0530

    Time: 100 ms (8.93%) | Memory: 56.5 MB (22.99%) - LeetSync

---
+++ 1375-find-palindrome-with-fixed-length/find-palindrome-with-fixed-length.java
@@ -0,0 +1,32 @@
+class Solution {
+    public long[] kthPalindrome(int[] queries, int intLength) {
+        int p = intLength % 2 == 0 ? (intLength / 2 ) - 1 : intLength / 2;
+        int leftLimit = (int) Math.pow(10, p);
+        int rightLimit = (int) Math.pow(10, p + 1) - 1;
+
+        long[] kthPalindromes = new long[queries.length];
+        String ans = "";
+        int k = 0;
+        for(int q : queries){
+            if(q <= (rightLimit - leftLimit) + 1){
+                ans = Integer.toString(leftLimit + q - 1);
+                String reverse = rev(ans, intLength - ans.length());
+                ans = ans + reverse;
+                kthPalindromes[k] = Long.parseLong(ans);
+                
+            }else{
+                kthPalindromes[k] = -1;
+            }
+            k++;
+        }
+        return kthPalindromes;
+    }
+    String rev(String s , int n){
+        String r = "";
+        for(int i = 0; i<n; i++){
+            char ch = s.charAt(i);
+            r = ch + r;
+        }
+        return r;
+    }
+}
