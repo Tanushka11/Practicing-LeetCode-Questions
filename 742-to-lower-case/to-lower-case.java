9932eae7d18375a0701baff7daeb36724cca779d
Tanushka Lalwani <90191834+Tanushka11@users.noreply.github.com>
2024-02-04 14:01:00 +0530

    Time: 1 ms (32.81%) | Memory: 40.8 MB (88.03%) - LeetSync

--- 742-to-lower-case/to-lower-case.java
+++ 742-to-lower-case/to-lower-case.java
@@ -1,5 +1,13 @@
 class Solution {
     public String toLowerCase(String s) {
-        return s.toLowerCase();
+        StringBuilder str = new StringBuilder();
+        for(int i = 0; i<s.length(); i++){
+            if(s.charAt(i) >= 65 && s.charAt(i) <= 90){
+                str.append((char) (s.charAt(i) + 32));
+            }else{
+                str.append(s.charAt(i));
+            }
+        }
+        return str.toString();
     }
 }
