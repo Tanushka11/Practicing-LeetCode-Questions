6b166326fc65e1b8d03a9a17b37b57d517f49235
Tanushka Lalwani <90191834+Tanushka11@users.noreply.github.com>
2024-02-09 15:04:20 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Find length of Loop/find-length-of-loop.java
@@ -0,0 +1,103 @@
+//{ Driver Code Starts
+// driver code
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Node
+{
+    int data;
+    Node next;
+    
+    Node(int x)
+    {
+        data = x;
+        next = null;
+    }
+}
+
+class GFG
+{
+    public static void makeLoop(Node head, Node tail, int x){
+        if (x == 0) return;
+        
+        Node curr = head;
+        for(int i=1; i<x; i++)
+            curr = curr.next;
+        
+        tail.next = curr;
+    }
+    
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t--> 0)
+        {
+            int n = sc.nextInt();
+            
+            int num = sc.nextInt();
+            Node head = new Node(num);
+            Node tail = head;
+            
+            for(int i=0; i<n-1; i++)
+            {
+                num = sc.nextInt();
+                tail.next = new Node(num);
+                tail = tail.next;
+            }
+            
+            int pos = sc.nextInt();
+            makeLoop(head, tail, pos);
+            
+            Solution x = new Solution();
+            System.out.println( x.countNodesinLoop(head) );
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+/*
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int d) {data = d; next = null; }
+}
+
+*/
+
+//Function should return the length of the loop in LL.
+
+class Solution
+{
+    //Function to find the length of a loop in the linked list.
+    static int countNodesinLoop(Node head)
+    {
+        //Add your code here.
+        Node fast = head;
+        Node slow = head;
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            
+            if(fast == slow){
+                break;
+            }
+        }
+        if(fast != slow || fast == null || fast.next == null){
+                return 0;
+            }
+        int count = 1;
+        slow = slow.next;
+        while(slow != fast){
+            count ++;
+            slow = slow.next;
+        }
+        return count;
+    }
+}
