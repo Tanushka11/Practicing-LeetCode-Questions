ca629ab1732cef08e100cf2333f9e7277fb9817f
Tanushka Lalwani <90191834+Tanushka11@users.noreply.github.com>
2024-02-11 23:20:17 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (64.95%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.java
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -9,17 +9,40 @@
  * }
  */
 class Solution {
+     ListNode th = null;
+     ListNode tt = null;
     public ListNode reverseList(ListNode head) {
-        if(head == null || head.next == null) return head;
-        ListNode prev = null;
-        ListNode pres = head;
-        ListNode next = head.next;
-        while(pres != null){
-            pres.next = prev;
-            prev = pres;
-            pres = next;
-            if(next != null) next = next.next;
+        // if(head == null || head.next == null) return head;
+        // ListNode prev = null;
+        // ListNode pres = head;
+        // ListNode next = head.next;
+        // while(pres != null){
+        //     pres.next = prev;
+        //     prev = pres;
+        //     pres = next;
+        //     if(next != null) next = next.next;
+        // }
+        // return prev;
+
+        // addfirst method
+        // one more method where we take four node trmphead,temptail and curr,forw
+
+        ListNode curr = head;
+        while(curr != null){
+            ListNode forward = curr.next;
+            curr.next = null;
+            addFirst(curr);
+            curr = forward;
         }
-        return prev;
+        return th;
+    }
+    void addFirst(ListNode node){
+        if(th == null){
+            th = node;
+            tt = node;
+        }else{
+            node.next = th;
+            th = node;
+        }
     }
 }
