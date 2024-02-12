d956d270617bf9ac518bd59778eb7af8ef6ab8e6
Tanushka Lalwani <90191834+Tanushka11@users.noreply.github.com>
2024-02-12 12:40:10 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (90.54%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
@@ -13,29 +13,47 @@ class Solution {
         if((head == null || head.next == null) && n == 1){
             return null;
         }
-        int len = findLen(head); 
-        if(n == len){
-            return head.next;
-        }
-        ListNode node = head;
-        for(int i = 0; i < len - n - 1; i++){
-            node = node.next;
-        }
-        ListNode d = node.next;
-        if( d!= null && d.next != null){
-            node.next = d.next;
-        }else{
-            node.next = null;
-        }
-        return head;
+    //     int len = findLen(head); 
+    //     if(n == len){
+    //         return head.next;
+    //     }
+    //     ListNode node = head;
+    //     for(int i = 1; i < len - n ; i++){
+    //         node = node.next;
+    //     }
+    //     ListNode d = node.next;
+    //     if( d!= null && d.next != null){
+    //         node.next = d.next;
+    //     }else{
+    //         node.next = null;
+    //     }
+    //     return head;
+    // }
+    // int findLen(ListNode head){
+    //     ListNode node = head;
+    //     int len = 0;
+    //     while(node != null){
+    //         node = node.next;
+    //         len++;
+    //     }
+    //     return len;
+
+    // one more method slow and fast 
+    ListNode slow = head;
+    ListNode fast = head;
+    for(int i = 1; i<= n; i++){
+        fast = fast.next;
     }
-    int findLen(ListNode head){
-        ListNode node = head;
-        int len = 0;
-        while(node != null){
-            node = node.next;
-            len++;
-        }
-        return len;
+    ListNode prev = null;
+    while(fast != null){
+        prev = slow;
+        slow = slow.next;
+        fast = fast.next;
+    }
+    if(prev == null){
+        return head.next;
+    }
+    prev.next = slow.next;
+    return head;
     }
 }
