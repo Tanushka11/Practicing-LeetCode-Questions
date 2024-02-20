01d20128473bc26f8baea4d94c5bbb7ba33615f8
Tanushka Lalwani <90191834+Tanushka11@users.noreply.github.com>
2024-02-20 16:56:41 +0530

    Added Solution - GfG to GitHub

---
+++ School/GCD of two numbers/gcd-of-two-numbers.java
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main(String args[])
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    int A = sc.nextInt();
+                    int B = sc.nextInt();
+                    Solution ob = new Solution();
+                    System.out.println(ob.gcd(A,B));
+                }
+        }
+}    
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int gcd(int A , int B) 
+    { 
+        //code here
+       if(A == 0){
+           return A;
+       }
+       if(B == 0){
+           return B;
+       }
+       if(A == B){
+           return A;
+       }
+       if(A > B){
+           return gcd(A-B, B);
+       }
+       return gcd(A, B-A);
+    } 
+}
