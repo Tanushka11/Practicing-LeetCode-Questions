ab50a68b49567b95b504024acb952d19520de4aa
Tanushka Lalwani <90191834+Tanushka11@users.noreply.github.com>
2024-02-26 15:24:47 +0530

    Time: 2 ms (46.59%) | Memory: 42.1 MB (83.11%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int myAtoi(String s) {
+        s = s.trim();
+        if(s.length() == 0){
+            return 0; 
+        }
+        long ans = 0;
+        boolean neg = false;
+        for(int i = 0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(i == 0){
+                if(ch == '-'){
+                    neg = true;
+                    continue;
+                } else if(ch =='+' ){
+                    neg = false;
+                    continue;
+                }
+            }
+            if(ch >= '0' && ch <= '9'){
+               int n = ch - '0';
+               ans = ans * 10 + n;
+               if(neg){
+                   long check = -ans;
+                   if(check < Integer.MIN_VALUE){
+                       return Integer.MIN_VALUE;
+                   }
+               }else{
+                   if(ans > Integer.MAX_VALUE){
+                       return Integer.MAX_VALUE;
+                   }
+               }
+            }else{
+                break;
+            }
+        }
+        if(neg){
+          ans = -ans;
+        }
+        return (int) ans;
+    }
+}
