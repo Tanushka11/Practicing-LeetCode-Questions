9f06b0004212ffee736bd3c8f24028cbf37248cd
Tanushka Lalwani <90191834+Tanushka11@users.noreply.github.com>
2024-03-01 22:02:41 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (96.47%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,13 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if (n == 0) return 1;
+        if (n == 1) return x;
+        if (n == -1) return 1 / x;
+        
+        double half = myPow(x, n / 2);
+        if (n % 2 == 0)
+            return half * half;
+        else
+            return half * half * (n < 0 ? 1 / x : x);
+    }
+}
