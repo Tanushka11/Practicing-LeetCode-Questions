99d1d0c98b8f5e6b2fcda5112172dfc35202e273
Tanushka Lalwani <90191834+Tanushka11@users.noreply.github.com>
2024-03-10 22:46:57 +0530

    Time: 1 ms (98.27%) | Memory: 54.6 MB (91.97%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -13,16 +13,37 @@ class Solution {
 
         // with hashmap O(n) + O(n)
 
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i= 0; i<nums.length; i++){
-            int v = map.getOrDefault(nums[i],0);
-            map.put(nums[i] , v+1);
-        }
-        for(int i =1; i<=nums.length; i++){
-            if(!map.containsKey(i)){
-                return i;
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        // for(int i= 0; i<nums.length; i++){
+        //     int v = map.getOrDefault(nums[i],0);
+        //     map.put(nums[i] , v+1);
+        // }
+        // for(int i =1; i<=nums.length; i++){
+        //     if(!map.containsKey(i)){
+        //         return i;
+        //     }
+        // }
+        // return nums.length + 1;
+
+        // cyclic sort 
+        int i = 0;
+        while(i < nums.length){
+            int ci = nums[i] -1;
+            if( nums[i] > 0 && nums[i] <= nums.length  && nums[i] != nums[ci] ){
+                int temp = nums[i];
+                nums[i] = nums[ci];
+                nums[ci] = temp;
+            }
+            else{
+                i++;
             }
         }
-        return nums.length+1;
+        for(int j =0; j<nums.length; j++){
+            if(nums[j] != j+1){
+                return j+1;
+            }
+        }
+        return  nums.length+1;
+
     } 
 }
