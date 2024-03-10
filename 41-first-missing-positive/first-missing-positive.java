295ea7c6e977b95cae67afaf13572172f17926d0
Tanushka Lalwani <90191834+Tanushka11@users.noreply.github.com>
2024-03-10 22:34:26 +0530

    Time: 12 ms (25.92%) | Memory: 60.4 MB (5.11%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -1,13 +1,28 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        Arrays.sort(nums);
-        int smallest = 1;
-        for(int i = 0; i<nums.length; i++){
-            if(nums[i] == smallest){
-                smallest++;
-            }
+        // bruteforce  O(nlogn + O(n))
+        // Arrays.sort(nums);
+        // int smallest = 1;
+        // for(int i = 0; i<nums.length; i++){
+        //     if(nums[i] == smallest){
+        //         smallest++;
+        //     }
             
+        // }
+        // return smallest;
+
+        // with hashmap O(n) + O(n)
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i= 0; i<nums.length; i++){
+            int v = map.getOrDefault(nums[i],0);
+            map.put(nums[i] , v+1);
         }
-        return smallest;
-    }
+        for(int i =1; i<=nums.length; i++){
+            if(!map.containsKey(i)){
+                return i;
+            }
+        }
+        return nums.length+1;
+    } 
 }
