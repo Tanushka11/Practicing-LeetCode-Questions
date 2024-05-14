6b9978b742e88006bac5052bbb49fa62ce4ed83a
Tanushka Lalwani <90191834+Tanushka11@users.noreply.github.com>
2024-05-14 16:57:23 +0530

    Time: 1 ms (99.82%) | Memory: 43.4 MB (40.01%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -7,8 +7,9 @@ class Solution {
         }
            // not picked
         helper(nums,i+1, cf, false);
+        // ignoring the same corrosponing elements which will generate duplicate subarrays
         if(i > 0 && (nums[i] == nums[i-1]) && (!prevElemPickedOrNot)){
-            return;
+           return;
         }
         // picked
         cf.add(nums[i]);
@@ -17,6 +18,11 @@ class Solution {
 
     }
     public List<List<Integer>> subsetsWithDup(int[] nums) {
+        // it is similar to subsets problem here we just need to sort
+        // the nums because we do not want duplicate subarray
+        // by sortion we will be able to check how many corrosponding elements
+        // are equal if they are equal then we will ignore them
+
         Arrays.sort(nums);
         helper(nums, 0,new ArrayList<>(), false);
         return ans;
