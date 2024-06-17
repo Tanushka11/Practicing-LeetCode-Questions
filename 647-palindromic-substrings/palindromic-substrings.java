40a9fb6a37b3400036b2a321c02e7d33e57a1393
Tanushka Lalwani <90191834+Tanushka11@users.noreply.github.com>
2024-06-17 15:52:35 +0530

    Time: 6 ms (74.57%) | Memory: 41.6 MB (49.46%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -1,30 +1,28 @@
 class Solution {
-    public Boolean isPallindrome(String s){
-        int i = 0;
-        int j = s.length() - 1;
-        while(i < j ){
-            if(s.charAt(i) != s.charAt(j)){
-                return false;
-            }
-            i++;
-            j--;
-        }
-        return true;
-    }
     public int countSubstrings(String s) {
-        int count = 1;
-        int len = s.length();
-        for(int i = 0; i < len - 1; i++){
-            String ss = s.charAt(i) + ""; 
-            count++;
-            for(int j = i+1; j<len; j++){
-                ss += s.charAt(j) + "";
-                if(isPallindrome(ss)) {
-                    
-                    count ++;
-                }
-            }
+        int count = 0;
+     int len = s.length();
+     String ss = "";
+     for(int mid  = 0; mid < len; mid++){
+        int i = mid - 1;
+        int j = mid + 1;
+         count ++;
+        while(i >= 0 && j < len && s.charAt(i) == s.charAt(j)){
+            i--;
+            j++;
+            count ++;
         }
-        return count;
+     }
+
+     for(int mid  = 0; mid < len; mid++){
+        int i = mid;
+        int j = mid + 1;
+        while(i >= 0 && j < len && s.charAt(i) == s.charAt(j)){
+            i--;
+            j++;
+            count ++;
+        }
+     }
+     return count;
     }
 }
