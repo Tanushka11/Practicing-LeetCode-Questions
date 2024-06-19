212e7e691a8d7e0b9fa63932f1a96feb4b3f95b4
Tanushka Lalwani <90191834+Tanushka11@users.noreply.github.com>
2024-06-19 18:02:56 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (91.27%) - LeetSync

---
+++ 54-spiral-matrix/spiral-matrix.java
@@ -0,0 +1,43 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> ans = new ArrayList<>();
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int minRow = 0;
+        int minCol = 0;
+        int maxRow = row - 1;
+        int maxCol = col - 1;
+        int count = 0;
+
+        while(minRow <= maxRow && minCol <= maxCol){
+            // top wall
+            for(int i = minRow, j = minCol; j <= maxCol && count < (row * col); j++){
+                ans.add(matrix[i][j]);
+                count++;
+            }
+            minRow++;
+            // right wall
+            for(int i = minRow,j = maxCol; i<=maxRow && count < (row * col) ; i++){
+                ans.add(matrix[i][j]);
+                count++;
+            }
+            maxCol--;
+            // bottom row
+            for(int i = maxRow , j = maxCol; j>=minCol && count < (row * col); j--){
+                ans.add(matrix[i][j]);
+                count++;
+            }
+            maxRow--;
+            // left wall
+            for(int i = maxRow, j = minCol; i>=minRow && count < (row * col); i--){
+                ans.add(matrix[i][j]);
+                count++;
+            }
+            minCol++;
+
+        }
+
+       return ans;
+
+    }
+}
