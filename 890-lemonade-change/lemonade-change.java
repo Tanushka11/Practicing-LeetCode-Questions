92b097bb5244c18aaaeeb0cab52234ffd041207a
Tanushka Lalwani <90191834+Tanushka11@users.noreply.github.com>
2024-07-05 18:07:28 +0530

    Time: 2 ms (94.81%) | Memory: 55.3 MB (95.52%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0;
+        int ten = 0;
+        int twenty = 0;
+        for(int i = 0; i<bills.length; i++){
+            if(bills[i] == 5){
+                five += 5;
+            }else if(bills[i] == 10 && five >= 5){
+                ten += 10;
+                five -= 5;
+            }else if(bills[i] == 20){
+                if(ten >= 10 && five >= 5){
+                    twenty += 20;
+                    ten -= 10;
+                    five -= 5;
+                }else if(five >= 15){
+                    five -= 15;
+                }else{
+                    return false;
+                }
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
