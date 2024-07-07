4c28bee36b2a72fa64648813b9f9c9e51d68e751
Tanushka Lalwani <90191834+Tanushka11@users.noreply.github.com>
2024-07-08 00:20:18 +0530

    Time: 2 ms (69.73%) | Memory: 42.7 MB (44.86%) - LeetSync

--- 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.java
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.java
@@ -1,28 +1,28 @@
-public class Solution {
+class Solution {
     public String shortestBeautifulSubstring(String s, int k) {
         int n = s.length();
-        String ans = "";
-        int i = 0, j = 0;
-        int c = 0;
-        
-        while (j < n) {
-            if (s.charAt(j) == '1') {
-                c++;
-            }
-            if (c == k) {
-                while (i < n && c == k) {
-                    String s1 = s.substring(i, j + 1);
-                    if (ans.isEmpty() || s1.length() < ans.length()) {
-                        ans = s1;
-                    } else if (s1.length() == ans.length()) {
-                        ans = (ans.compareTo(s1) < 0) ? ans : s1;
-                    }
-                    if (s.charAt(i) == '1') {
-                        c--;
-                    }
-                    i++;
+       int i = 0;
+       int j = 0;
+       int count = 0;
+       String ans = "";
+        while(j < n){
+            if(s.charAt(j) == '1') count++;
+           if(count == k){
+             while(i<n && count == k){
+                String str = s.substring(i,j+1);
+                if(ans.isEmpty() || (str.length() < ans.length())){
+                    ans = str;
                 }
+                else if(str.length() == ans.length()) {
+                //     int v1 = Integer.valueOf(str);
+                //     int v2 = Integer.valueOf(ans);
+                //     ans = (v1 < v2) ? str : ans;
+                   ans = (ans.compareTo(str) < 0) ? ans : str;
+                }
+                if(s.charAt(i) == '1') count--;
+                i++;
             }
+           }
             j++;
         }
         return ans;
