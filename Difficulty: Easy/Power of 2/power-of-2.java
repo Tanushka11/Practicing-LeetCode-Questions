15e2fecc11578d311629caca80a21944087431c6
Tanushka Lalwani <90191834+Tanushka11@users.noreply.github.com>
2024-07-24 14:11:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Power of 2/power-of-2.java
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+// //Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Driver_class {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine()); // testcases
+
+        while (t-- > 0) {
+            // input a number n
+            long n = Long.parseLong(read.readLine());
+
+            // if n is less than equal to zero then
+            // it can't be a power of 2 so we print No
+            if (new Solution().isPowerofTwo(n) == true)
+                System.out.println("true");
+            else
+                System.out.println("false");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+
+    // Function to check if given number n is a power of two.
+    public static boolean isPowerofTwo(long n) {
+
+        // Your code here
+        while(n > 1){
+            if(n % 2 != 0) return false;
+            n = n/2;
+        }
+        if(n == 1) return true;
+        return false;
+    }
+}
