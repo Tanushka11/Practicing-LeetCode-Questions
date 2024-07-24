25725e847eda6987363731986864d41a9a558829
Tanushka Lalwani <90191834+Tanushka11@users.noreply.github.com>
2024-07-24 22:10:48 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (58.05%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,23 +1,16 @@
 class Solution {
     public List<List<Integer>> ans = new ArrayList<>();
-    public void helper(int[] nums, int i, List<Integer> combination){
-        //base case
-        if(i == nums.length){
-           ans.add(new ArrayList <>(combination));
-           return;
-        }
-       
-
-        // pick
-         combination.add(nums[i]);
-         helper(nums,i+1,combination);
-         combination.remove(combination.size() - 1);
-        
-          //do not pick
-           helper(nums,i+1,combination);
-    }
     public List<List<Integer>> subsets(int[] nums) {
-       helper(nums,0, new ArrayList<>());
-       return ans;
+        int n = nums.length;
+        int total = (int) Math.pow(2,n);
+        for(int i = 0; i < total; i++){
+            List<Integer> subset = new ArrayList<>();
+            for(int bitPosition = 0; bitPosition < n; bitPosition++){
+                int mask = 1 << bitPosition;
+                if((i & mask) != 0) subset.add(nums[bitPosition]);
+            }
+            ans.add(subset);
+        }
+        return ans;
     }
 }
